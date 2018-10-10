80aadf314597463c1141baea3067135c59a760fa
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-10-10 17:23:54 +0100

    Fixed typo in MyRectangle

--- primitives/MyRectangle.js
+++ primitives/MyRectangle.js
@@ -22,10 +22,10 @@ class MyRectangle extends CGFobject
 	{
 			
 		this.vertices = [
-			x1, y1, 0,
-			x1, y2, 0,
-			x2, y1, 0,
-			x2, y2, 0
+			this.x1, this.y1, 0,
+			this.x1, this.y2, 0,
+			this.x2, this.y1, 0,
+			this.x2, this.y2, 0
 		];
 
 		this.normals = [
