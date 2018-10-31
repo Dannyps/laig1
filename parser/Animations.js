29142982c6bbd07188f16b7ddeaec76eac697fff
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-10-31 18:13:29 +0000

    Added linear animations parser

--- parser/Animations.js
+++ parser/Animations.js
@@ -1,16 +1,13 @@
 'use strict';
 
 /////// UPDATE THESE DOCS
-/**
- * @typedef animation
- * @type {object}
- * @property {(parsedRotate | parsedScale | parsedTranslate)[]} transformation - Array of transformations. Might be empty if it hasn't own transformations 
- * @property children:
- * @property {string[]} primitivesID
- * @property {string[]} animationsID
- * @property {string[]} materials
- * @property texture: any;
- */
+ /**
+  * @typedef linearAnimation
+  * @type {object}
+  * @property {string} type - "Linear"
+  * @property {number} dadas - 
+  * @property {Array.<{xx:number, yy:number, zz:number}} ctrlPoints 
+  */
 class AnimationsParser extends GenericParser {
     /**
      * Parser for <animations> tag
@@ -23,7 +20,7 @@ class AnimationsParser extends GenericParser {
         this.animations = new Map();
     }
 
-    getParsedA  nimations() {
+    getParsedAnimations() {
         return this.animations;
     }
 
@@ -33,11 +30,21 @@ class AnimationsParser extends GenericParser {
      * @return {number} Returns -1 if no animations are defined, 0 otherwise
      */
     parse(animationsNode) {
-        // find all animation elements and parse them
-        let animationsCollection = animationsNode.getElementsByTagName('animation');
-        Array.prototype.forEach.call(animationsCollection, (compEl) => {
-            this._parseanimations(compEl);
-        });
+        if(animationsNode.tagName != 'animations') throw "Unexpected element";
+
+        for(let animation of animationsNode.children) {
+            let parsedAnimation = null;
+            switch(animation.tagName) {
+                case 'linear':
+                    // TODO check return
+                    this._parseLinearAnimation(animation);
+                    break;
+                case 'circular':
+                    break;
+                default:
+                    this.onXMLMinorError(`Unknown ${animation.tagName} under <animations> tag`);
+            }
+        }
 
         // ensure at least one animation is defined
         if (this.animations.size === 0) {
@@ -48,5 +55,45 @@ class AnimationsParser extends GenericParser {
         return 0;
     }
 
+    /**
+     * Parses linear animation tags
+     * @param {Element} linearEl The <linear> DOM element to be parsed
+     * @return {(>|null)} Returns null upon error, otherwise an array of control points
+     */
+    _parseLinearAnimation(linearEl) {
+        if(linearEl.tagName != 'linear') throw "Unexpected element";
 
+        // parse the attributes
+        let parsedAttrs = this._parseAttributes(linearEl, {id: 'ss', span: 'ff'});
+        if(parsedAttrs === null)
+            return null; // failed to parse attributes
+
+        // check animation ID uniqueness
+        if(this.animations.has(parsedAttrs.id)) {
+            this.onXMLMinorError(`An animation with id ${parsedAnimation.id} already exists!`);
+            return null; // conflict
+        }
+
+        let ctrlPoints = []; // array of control points
+        
+        // parse the control points
+        for(let ctrlPoint of linearEl.children) {
+            if(ctrlPoint.tagName != 'controlpoint') {
+                this.onXMLError(`Unexpected element <${ctrlPoint.tagName}> under <linear id=${parsedAttrs.id}>`);
+                return null;
+            }
+
+            let coords = this._parseAttributes(ctrlPoint, {xx: 'ff', yy: 'ff', zz: 'ff'});
+            if(coords === null)
+                return null;
+
+            ctrlPoints.push(coords);
+        }
+
+        // store the animatin
+        this.animations.set(parsedAttrs.id, {
+            type: linearEl.tagName,
+            ctrlPoints: ctrlPoints,
+        });
+    }
 }
