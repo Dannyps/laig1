9b7a18e9f50e4a74f8c7798490fd7e35935f19e5
Daniel Silva <danny@dannyps.net>
2018-11-16 12:47:58 +0000

    Make nurbs planes have the right size

--- primitives/Plane.Nurbs.js
+++ primitives/Plane.Nurbs.js
@@ -7,14 +7,14 @@ class Plane_Nurbs extends CGFobject {
 
         let controlVertexes=[	// U = 0
             [ // V = 0..1;
-                 [-1.0, -1.0, 0.0, 1 ],
-                 [-1.0,  1.0, 0.0, 1 ]
+                 [-1/2, -1/2, 0.0, 1 ],
+                 [-1/2,  1/2, 0.0, 1 ]
                 
             ],
             // U = 1
             [ // V = 0..1
-                 [ 1.0, -1.0, 0.0, 1 ],
-                 [ 1.0,  1.0, 0.0, 1 ]							 
+                 [ 1/2, -1/2, 0.0, 1 ],
+                 [ 1/2,  1/2, 0.0, 1 ]							 
             ]
         ]
         let obj = new Nurbs(this.scene, 1, 1, controlVertexes, npartsu, npartsv);
