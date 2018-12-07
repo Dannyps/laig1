3461d6f4a8aca8dfea650da66f5d2298f5b7b83d
Fabio Daniel Reis Gaspar <fabiodanielreis@hotmail.com>
2018-12-07 11:54:20 +0000

    Added yet another primitive

---
+++ primitives/BoardSpot.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a board spot
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+class BoardSpot extends Plane_Nurbs
+{
+	constructor(scene) 
+	{
+        super(scene, 10, 10);        
+    };
+};
