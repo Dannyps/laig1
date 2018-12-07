d5b669b2896ecc0124c05471c882ca7c1081673c
Daniel Silva <danny@dannyps.net>
2018-12-07 10:52:37 +0000

    Add example

--- LightingScene.js
+++ LightingScene.js
@@ -1,113 +1,137 @@
-function LightingScene() {
-	CGFscene.call(this);
-	this.texture = null;
-	this.appearance = null;
-	this.surfaces = [];
-	this.translations = [];
-}
 
-LightingScene.prototype = Object.create(CGFscene.prototype);
-LightingScene.prototype.constructor = LightingScene;
+class LightingScene extends CGFscene
+{
+	init(application)
+	{
+		super.init(application);
 
-LightingScene.prototype.init = function (application) {
-	CGFscene.prototype.init.call(this, application);
+		this.initCameras();
 
-	this.initCameras();
+		this.initLights();
 
-	this.initLights();
+		this.gl.clearColor(1.0, 1.0, 1.0, 1.0);
+		this.gl.clearDepth(100.0);
+		this.gl.enable(this.gl.DEPTH_TEST);
+		this.gl.enable(this.gl.CULL_FACE);
+		this.gl.depthFunc(this.gl.LEQUAL);
 
-	this.gl.clearColor(0, 0, 0, 1.0);
-	this.gl.clearDepth(10000.0);
-	this.gl.enable(this.gl.DEPTH_TEST);
-	this.gl.enable(this.gl.CULL_FACE);
-	this.gl.depthFunc(this.gl.LEQUAL);
+		this.axis = new CGFaxis(this);
 
-	this.axis = new CGFaxis(this);
+		// Scene elements
+		
+		this.suzanne = new CGFOBJModel(this, 'models/suzanne.obj');
+		this.male = new CGFOBJModel(this, 'models/male.obj');
+		this.navigator = new CGFOBJModel(this, 'models/navigator.obj', true);
+		
+		// Materials
+		this.materialDefault = new CGFappearance(this);
 
-	this.appearance = new CGFappearance(this);
-	this.appearance.setAmbient(0.3, 0.3, 0.3, 1);
-	this.appearance.setDiffuse(0.7, 0.7, 0.7, 1);
-	this.appearance.setSpecular(0.0, 0.0, 0.0, 1);
-	this.appearance.setShininess(120);
+		this.enableTextures(true);
 
-	this.objects = [
-		new CGFplane(this),
-		new CGFplane(this),
-		new CGFplane(this),
-		new CGFplane(this)
-	];
+	};
 
-	this.setPickEnabled(true);
+	initCameras() {
+		this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(30, 15, 30), vec3.fromValues(0, 5, 0));
+	};
 
-};
+	initLights() {
+		this.setGlobalAmbientLight(0, 0 ,0, 1);
 
-LightingScene.prototype.initLights = function () {
-	this.lights[0].setPosition(1, 1, 1, 1);
-	this.lights[0].setAmbient(0.1, 0.1, 0.1, 1);
-	this.lights[0].setDiffuse(0.9, 0.9, 0.9, 1);
-	this.lights[0].setSpecular(0, 0, 0, 1);
-	this.lights[0].enable();
-	this.lights[0].update();
-};
+		var height = 50;
+		var spread = 10;
+
+		this.lights[0].setPosition(spread, height, spread, 1);
+		this.lights[0].setAmbient(0, 0, 0, 1);
+		this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[0].setSpecular(1.0, 1.0, 1.0, 1.0);
+		this.lights[0].enable();
+
+		this.lights[1].setPosition(spread, height, -spread, 1.0);
+		this.lights[1].setAmbient(0, 0, 0, 1);
+		this.lights[1].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[1].setSpecular(1.0, 1.0, 1.0, 1.0);
+		this.lights[1].enable();
+
+		this.lights[2].setPosition(-spread, height, -spread, 1.0);
+		this.lights[2].setAmbient(0, 0, 0, 1);
+		this.lights[2].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[2].setSpecular(1.0, 1.0, 1.0, 1.0);
+		this.lights[2].enable();
+
+		this.lights[3].setPosition(-spread, height, spread, 1.0);
+		this.lights[3].setAmbient(0, 0, 0, 1);
+		this.lights[3].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[3].setSpecular(1.0, 1.0, 1.0, 1.0);
+		this.lights[3].enable();
+
+	};
+
+	updateLights() {
+		for (var i = 0; i < this.lights.length; i++)
+			this.lights[i].update();
+	};
+
+	display() {
+
+		// ---- BEGIN Background, camera and axis setup
+
+		// Clear image and depth buffer everytime we update the scene
+		this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+		this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+		// Initialize Model-View matrix as identity (no transformation)
+		this.updateProjectionMatrix();
+		this.loadIdentity();
+
+		// Apply transformations corresponding to the camera position relative to the origin
+		this.applyViewMatrix();
+
+		// Update all lights used
+		this.updateLights();
+
+		// Draw axis
+		this.axis.display();
+
+		this.materialDefault.apply();
+
+		// ---- END Background, camera and axis setup
 
 
-LightingScene.prototype.initCameras = function () {
-	this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(15, 14, 13), vec3.fromValues(0, 0, 0));
-};
+		// ---- BEGIN Primitive drawing section
 
-LightingScene.prototype.logPicking = function () {
-	if (this.pickMode == false) {
-		if (this.pickResults != null && this.pickResults.length > 0) {
-			for (var i = 0; i < this.pickResults.length; i++) {
-				var obj = this.pickResults[i][0];
-				if (obj) {
-					var customId = this.pickResults[i][1];
-					console.log("Picked object: " + obj + ", with pick id " + customId);
-				}
-			}
-			this.pickResults.splice(0, this.pickResults.length);
-		}
-	}
-}
-
-
-LightingScene.prototype.display = function () {
-	this.logPicking();
-	this.clearPickRegistration();
-
-	// Clear image and depth buffer every time we update the scene
-	this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
-	this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
-	this.gl.enable(this.gl.DEPTH_TEST);
-
-	// Initialize Model-View matrix as identity (no transformation
-	this.updateProjectionMatrix();
-	this.loadIdentity();
-
-	// Apply transformations corresponding to the camera position relative to the origin
-	this.applyViewMatrix();
-
-	//this.scale(5,5,5);
-
-	// Update all lights used
-	this.lights[0].update();
-
-	// Draw axis
-	this.axis.display();
-
-	this.appearance.apply();
-
-	this.rotate(Math.PI / 2.0, 1, 0, 0);
-
-	// draw objects
-	for (i = 0; i < this.objects.length; i++) {
+		// suzanne
 		this.pushMatrix();
 
-		this.translate(i * 2, 0, 0);
-		this.registerForPick(i + 1, this.objects[i]);
+		this.translate(5, 5, 0);
 
-		this.objects[i].display();
+		this.suzanne.display();
+
 		this.popMatrix();
-	}
 
-}
+
+		// male
+		this.pushMatrix();
+
+		this.scale(0.5, 0.5, 0.5);
+
+		this.male.display();
+
+		this.popMatrix();
+
+
+		// navigator
+		this.pushMatrix();
+
+		this.translate(-10, 0, 0);
+		this.scale(2, 2, 2);
+		this.translate(0, 3, 0);
+		this.rotate(-Math.PI/2, 1, 0, 0);
+
+		this.navigator.display();
+
+		this.popMatrix();
+
+		// ---- END Primitive drawing section
+
+	};
+};
